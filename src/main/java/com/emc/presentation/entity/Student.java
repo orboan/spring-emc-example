f4779d74e2d9e8110ed09e20e270f6a13b53a29c
Oriol Boix Anfosso <orboan@ilg.cat>
2022-10-25 10:46:45 +0000

    Added XML DI Student entity. closes #3

---
+++ src/main/java/com/emc/presentation/entity/Student.java
@@ -0,0 +1,58 @@
+package com.emc.presentation.entity;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private String surname;
+	private String address;
+	
+	public Student() {
+		
+	}
+	
+	public Student(int id, String name, String surname, String address) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.address = address;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String toString() {
+		String separator = " ";
+		return new StringBuilder().append(Integer.toString(id))
+				.append(separator)
+				.append(name)
+				.append(separator)
+				.append(surname)
+				.append(separator)
+				.append(address)				
+				.toString();
+	}
+	
+}
