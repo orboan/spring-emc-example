ca6e879b8252591b9e43ca05d2b1b593a0a6f175
Oriol Boix Anfosso <orboan@ilg.cat>
2022-10-25 09:17:35 +0000

    Added XML DI. closes #1

---
+++ src/main/java/com/emc/presentation/xml/HelloWorld.java
@@ -0,0 +1,39 @@
+package com.emc.presentation.xml;
+
+public class HelloWorld {
+	private String hello;
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		
+	}
+
+	/**
+	 * 
+	 * @param hello
+	 */
+	public HelloWorld(String hello) {
+		this.hello = hello;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHello() {
+		return hello;
+	}
+	
+	/**
+	 * 
+	 * @param hello
+	 */
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+
+	
+}
+
